d15011737b67d4705a3365ecfd8819ec10792317
luckydz <algerienvenere@gmail.com>
2022-05-18 21:13:09 +0200

    Structure dossier + début de page home + navigation

---
+++ vue/navbar.php
@@ -0,0 +1,51 @@
+<?php
+    $links = array(
+        [
+            "href" => "./router.php/mondes",
+            "name" => "Mondes"
+        ],
+        [
+            "href" => "./router.php/niveaux",
+            "name" => "Niveaux"
+        ],
+        [
+            "href" => "./router.php/personnages",
+            "name" => "Personnages"
+        ],
+        [
+            "href" => "./router.php/ennemis",
+            "name" => "Ennemis"
+        ],
+        [
+            "href" => "./router.php/smoothies",
+            "name" => "Smoothies"
+        ],
+        [
+            "href" => "./router.php/ingredients",
+            "name" => "Ingrédients"
+        ],
+        [
+            "href" => "./router.php/exercices",
+            "name" => "Exercices"
+        ],
+        [
+            "href" => "./router.php/smoothies",
+            "name" => "Muscles"
+        ],
+    );
+?>
+
+<nav class="navbar bg-white">
+    <div class="container">
+        <?php
+            foreach ($links as $link) {
+                echo '<a class="navbar-brand" href="'.$link["href"].'">'.$link["name"].'</a>';
+            }
+        ?>
+    </div>
+</nav>
+
+
+<!-- CSS Lié à la vue -->
+<link rel="stylesheet" href="./assets/css/layouts/navbar.css">
+<!-- On appelle ici plutot que dans le css -->
