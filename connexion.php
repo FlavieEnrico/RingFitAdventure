f9506db2073b0c8124ff4144daf5ab11522691c7
EliseDugue <elise.dugue@mmibordeaux.com>
2022-06-01 15:46:44 +0200

    Create connexion.php

---
+++ connexion.php
@@ -0,0 +1,10 @@
+<?php
+
+function connection() {
+    $cnx = new PDO('mysql:host=localhost;dbname=flavie_enrico_db', 'root', '');
+    if(!$cnx) {
+        die('Connection failed');
+    }
+    return $cnx;
+    
+}
